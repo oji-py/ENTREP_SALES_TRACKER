81a8eae588b5f3c57f4c43c219ef52c931785427
oji-py <128983643+oji-py@users.noreply.github.com>
2023-05-24 00:43:01 +0800

    Add files via upload

---
+++ entrep_track.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+
+st.header("WAFI-SAN ORDER TRACKER")
+st.markdown("---")
+# Functions
+def get_order_variant(order):
+    if order == "WAFI":
+        variant = entry_form.radio("WAFI VARIANT:", ("PLAIN", "W/ MAPLE"))
+    else:
+        variant = entry_form.radio("WAFI-SAN", ("HOT & SPICY", "KETCHUP & MAYO", "MAPLE SYRUP", "NO SAUCE"))
+    return order, variant
+
+# Form
+st.sidebar.header("ORDER INPUT")
+entry_form = st.sidebar.form("entrep_form")
+customer_name = entry_form.text_input("Enter the customer's name")
+order_choice = entry_form.radio("Customer Order: ", ('WAFI', 'WAFI-SAN'))
+item = get_order_variant(order_choice)
+num_order = entry_form.radio("NUMBER OF ORDERS: ", ("1", "2", "3", "4", "5"))
+total_amount = entry_form.number_input("Amount Paid")
+submit = entry_form.form_submit_button("Submit")
+
+# List
+sales_list = pd.read_csv("entrep_.csv")
+st.header("Sales Tracker")
+st.write(sales_list)
+if submit:
+    st.write(customer_name, order_choice, num_order)
+    sales_dict = {"Name": customer_name, "Item": item, "No. of items": num_order, "Total amount": total_amount}
+    st.write(sales_dict)
+    sales_entry = sales_list.append(sales_dict, ignore_index=True)
+    st.write(sales_entry)
+    sales_entry.to_csv("entrep_.csv", index=False)
+    
